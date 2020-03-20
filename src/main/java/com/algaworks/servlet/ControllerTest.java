80f0070d8d19c915e14e2c636a6b3278c7618fe1
Victor Bello <victor.bello@voxage.com.br>
2020-03-20 14:40:32 -0300

    Alterações no projeto
    - Adição do gitignore para que arquivos que não precisam ser rastreados pelo git sigam sendo comitados. Toda vez que o projeto é importado para uma IDE esses arquivos são alterados e não fazem diferença para outro dev.
    
    - Alteração do pom.xml para que a versao 2.3 do jsf funcione com o tomcat. O tomcat, por ser um container, não contem algumas dependencias de um servidor de aplicação (wildfly) portanto, é necessário que essas dependencias sejam adicionadas manualmente.
    
    - Atualização do pom para utilizar a versão mais recente, disponivel gratuitamente, do primefaces.
    
    - Substituição do servlet TestServlet pela classe Jsf23Activator. Essa classe é uma ativação necessária no jsf 2.3 para funcionar com capatibilidade com a versão anterior (https://stackoverflow.com/questions/53171633/facescontext-not-injectable-in-wildfly-14-with-jsf-2-3-mojarra-main-module/53172876#53172876)
    
    - Alteração do arquivo web.xml que estava utilizando servlet diretamente quando deveria indicar a servlet propria do JSF.
    	Alteração para leitura do index.xhtml como arquivo inicial. Sendo assim, ao acessar o context da aplicação ela redireciona diretamente para esta pagina.
    
    - remoção do arquivo jsp.
    
    - Desenvolvimento de um formulário no arquivo index.xhtml e um controlador jsf para exemplificação
    
    - Adição do arquivo beans.xml para utilização de CDI
    
    - Adição do arquivo faces-config.xml utilizado pelo jsf e que permite outras configurações caso necessario

---
+++ src/main/java/com/algaworks/servlet/ControllerTest.java
@@ -0,0 +1,32 @@
+package com.algaworks.servlet;
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+public class ControllerTest implements Serializable {
+	private String nome;
+
+	@PostConstruct
+	public void init() {
+		System.out.println("Iniciando controlador - tudo poderá ser carregado aqui");
+	}
+
+	public void doAction() {
+		System.out.println("Efetuando aplicação dos valores e lendo");
+		System.out.println("O nome digitado foi: " + getNome());
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
